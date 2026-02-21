4177e6201bbae547c92caf9eb84977f00f5cca3c
mammumalluru <mamatha.malluru@outlook.com>
2026-02-21 20:45:23 +0530

    ingress

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ resource "aws_instance" "test_instance" {
     ami = "ami-0f3caa1cf4417e51b"
     instance_type = "t2.small"
     key_name = "MC"
+    subnet_id = aws_subnet.test_subnet.id
     vpc_security_group_ids = [aws_security_group.test_sg.id]
     user_data = file("userdata.sh")
 
